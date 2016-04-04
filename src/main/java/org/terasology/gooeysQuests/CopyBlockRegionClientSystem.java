c76b4f5ef288f21b8565600e5785a43589914447
Florian Köberle <florian@fkoeberle.de>
2016-04-04 21:19:54 +0200

    Add a tool for copying block regions to clipboard.

---
+++ src/main/java/org/terasology/gooeysQuests/CopyBlockRegionClientSystem.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.gooeysQuests;
+
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.event.ReceiveEvent;
+import org.terasology.entitySystem.systems.BaseComponentSystem;
+import org.terasology.entitySystem.systems.RegisterMode;
+import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.gooeysQuests.quests.dungeon.CopyBlockRegionResultEvent;
+import org.terasology.logic.clipboard.ClipboardManager;
+import org.terasology.registry.In;
+import org.terasology.world.block.Block;
+
+/**
+ * Handles the result of the activation of the copyBlockRegionTool item.
+ */
+@RegisterSystem(RegisterMode.CLIENT)
+public class CopyBlockRegionClientSystem extends BaseComponentSystem {
+
+    @In
+    private ClipboardManager clipboardManager;
+
+    @ReceiveEvent
+    public void onCopyBlockRegionResultEvent(CopyBlockRegionResultEvent event, EntityRef entity) {
+        StringBuilder sb = new StringBuilder();
+        for (Block block : event.getBlocks()) {
+            sb.append('"');
+            sb.append(block.getURI().toString());
+            sb.append("\", ");
+        }
+        clipboardManager.setClipboardContents(sb.toString());
+    }
+}
