bcf5a1cec2cfec4372fa884e2a4d5a6aa60782c8
DarkWeird <foxflameless@gmail.com>
2021-07-09 16:53:17 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components.

--- src/main/java/org/terasology/gooeysQuests/api/SpawnMagicBuildParticlesComponent.java
+++ src/main/java/org/terasology/gooeysQuests/api/SpawnMagicBuildParticlesComponent.java
@@ -1,22 +1,9 @@
-/*
- * Copyright 2016 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.gooeysQuests.api;
 
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.world.block.BlockRegion;
+import org.terasology.gestalt.entitysystem.component.Component;
 import org.terasology.structureTemplates.events.SpawnStructureEvent;
 import org.terasology.structureTemplates.events.StructureBlocksSpawnedEvent;
 
@@ -24,6 +11,6 @@ import org.terasology.structureTemplates.events.StructureBlocksSpawnedEvent;
  * If a entity with this component gets a {@link SpawnStructureEvent} (that triggers a
  * {@link StructureBlocksSpawnedEvent}) then particles will be spawned in the given region.
  */
-public class SpawnMagicBuildParticlesComponent implements Component {
+public class SpawnMagicBuildParticlesComponent implements Component<SpawnMagicBuildParticlesComponent> {
     public BlockRegion region;
 }
