bb65b5aa39ea2f38ac8b5100e690543a7bb15b56
Tobias Nett <skaldarnar@googlemail.com>
2022-01-13 23:04:15 +0100

    refactor: Adjust for Behaviors#88
    Adjust for refactorings done in Terasology/Behaviors#88.
    
    The changes resulted from automatic refactorings in IntelliJ and are **untested**.
    The IDE support only helps with Java classes, so there might be assets or other references that slipped through.
    
    At least Omega compiles with these changes.

--- src/main/java/org/terasology/gooeysQuests/GooeySpawnSystem.java
+++ src/main/java/org/terasology/gooeysQuests/GooeySpawnSystem.java
@@ -20,7 +20,7 @@ import org.joml.Quaternionf;
 import org.joml.RoundingMode;
 import org.joml.Vector3f;
 import org.joml.Vector3i;
-import org.terasology.behaviors.components.NPCMovementComponent;
+import org.terasology.module.behaviors.components.NPCMovementComponent;
 import org.terasology.engine.entitySystem.entity.EntityBuilder;
 import org.terasology.engine.entitySystem.entity.EntityManager;
 import org.terasology.engine.entitySystem.entity.EntityRef;
